39e480f70b7daa2df654e49681ca4ebecd78866d
Katharina Buchthal <kabu00002@stud.uni-saarland.de>
2023-06-07 16:55:54 +0200

    updated filters

--- src/docking_utils.py
+++ src/docking_utils.py
@@ -13,7 +13,7 @@ from rdkit.Chem import rdMolAlign
 from rdkit.Chem.PropertyMol import PropertyMol
 from functools import reduce
 import pandas as pd
-from kinfraglib import utils
+from kinfraglib import utils, filters
 from brics_rules import is_brics_bond
 
 PATH_FLEXX = Path('./FlexX.app/Contents/MacOS/FlexX')
@@ -53,12 +53,55 @@ def core_docking(path_fragment, path_config, path_output, print_output=False):
 
     # read core fragments poses from sdf
     docked_core_fragments = []
-    if os.stat(str(path_output)).st_size:
+    if os.stat(str(path_output)).st_size:   # only acces reult file if at least one pose was generated
         for i, molecule in enumerate(Chem.SDMolSupplier(str(path_output))):
             molecule.SetProp('pose', str(i))
             docked_core_fragments.append(molecule)
     
     return docked_core_fragments
+
+def hyde_scoring(path_docking_results, path_config, path_output, print_output=False):
+    """
+    runs hydescoring
+
+    Returns
+    ----------
+    List of all poses (docking result) as Mol (Properties: BIOSOLVEIT.DOCKING_SCORE, pose)
+
+    Parameters
+    ----------
+    path_fragment: pathlib.path
+        Path to fragment sdf-file
+    path_config: pathlib.path
+        Path to hyde-config file.
+    path_output: pathlib.path
+        Path to output file
+    """
+    output_text = subprocess.run(
+        [
+            str('./Hydescorer.app/Contents/MacOS/hydescorer'),
+            "-i",
+            str(path_docking_results),
+            "--binding-site-definition",
+            str(path_config),
+            "-o",
+            str(path_output),
+        ],
+        capture_output=True
+    )
+    if print_output:
+        print(output_text.stdout)
+
+    # read results from sdf
+    opt_fragments = []
+    if os.stat(str(path_output)).st_size and os.stat(str(path_docking_results)).st_size:  # only acces reult file if at least one pose was generated
+        for i, molecules in enumerate(zip(Chem.SDMolSupplier(str(path_docking_results)), Chem.SDMolSupplier(str(path_output)))):
+            molecule_docking, molecule_opt = molecules
+            molecule_opt.SetProp('pose', str(i))
+            molecule_opt.SetProp('BIOSOLVEIT.DOCKING_SCORE', molecule_docking.GetProp('BIOSOLVEIT.DOCKING_SCORE'))
+            opt_fragments.append(molecule_opt)
+    
+    return opt_fragments
 
 def template_docking(path_fragment, path_template, path_config, path_output, print_output=False):
     """
@@ -66,20 +109,18 @@ def template_docking(path_fragment, path_template, path_config, path_output, pri
 
     Returns
     ----------
-    List of all poses (docking result) as Mol (Properties: docking-score, pose)
+    List of all poses (docking result) as Mol (Properties: BIOSOLVEIT.DOCKING_SCORE, pose)
 
     Parameters
     ----------
     path_fragment: pathlib.path
-        Path to core-fragment sdf-file
+        Path to fragment sdf-file
     path_template: pathlib.path
         Path to template-fragment sdf-file
     path_config: pathlib.path
         Path to FlexX-config file.
     path_output: pathlib.path
         Path to output file
-    template_pose: int
-        ID of template pose  
     """
     # template docking
     output_text = subprocess.run(
@@ -105,7 +146,7 @@ def template_docking(path_fragment, path_template, path_config, path_output, pri
 
     # read fragments poses from sdf
     docked_fragments = []
-    if not os.path.exists(str(path_output)):
+    if not os.path.exists(str(path_output)): # If docking failed TODO docking shouldn't fail
         return docked_fragments
     if os.stat(str(path_output)).st_size:
         for i, molecule in enumerate(Chem.SDMolSupplier(str(path_output))):
@@ -114,57 +155,14 @@ def template_docking(path_fragment, path_template, path_config, path_output, pri
     
     return docked_fragments
 
-#   ===================  Class-based ================
+#   ===================  CLASS DEFINITIONS ================
 
 class Pose:
-    def __init__(self, ROMol, docking_score, hyde_score = None):
+    def __init__(self, ROMol, docking_score):
         self.docking_score = docking_score
         self.ROMol = ROMol
-        self.hyde_score = hyde_score
     def _is_valid_operand(self, other):
         return hasattr(other, 'docking_score')
-    def __lt__(self, other):
-        if not self._is_valid_operand(other):
-            return NotImplemented
-        if self.hyde_score != None and other.hyde_score != None:
-            return self.hyde_score < other.hyde_score
-        else:
-            return self.docking_score < other.docking_score
-    def __le__(self, other):
-        if not self._is_valid_operand(other):
-            return NotImplemented
-        if self.hyde_score != None and other.hyde_score != None:
-            return self.hyde_score <= other.hyde_score
-        else:
-            return self.docking_score <= other.docking_score
-    def __gt__(self, other):
-        if not self._is_valid_operand(other):
-            return NotImplemented
-        if self.hyde_score != None and other.hyde_score != None:
-            return self.hyde_score > other.hyde_score
-        else:
-            return self.docking_score > other.docking_score
-    def __ge__(self, other):
-        if not self._is_valid_operand(other):
-            return NotImplemented
-        if self.hyde_score != None and other.hyde_score != None:
-            return self.hyde_score > other.hyde_score
-        else:
-            return self.docking_score > other.docking_score
-    def __eq__(self, other):
-        if not self._is_valid_operand(other):
-            return NotImplemented
-        if self.hyde_score != None and other.hyde_score != None:
-            return self.hyde_score == other.hyde_score
-        else:
-            return self.docking_score == other.docking_score
-    def __ne__(self, other):
-        if not self._is_valid_operand(other):
-            return NotImplemented
-        if self.hyde_score != None and other.hyde_score != None:
-            return self.hyde_score != other.hyde_score
-        else:
-            return self.docking_score != other.docking_score
 
 class Ligand:
     def __init__(self, ROMol, fragment_ids, recombination):
@@ -175,9 +173,9 @@ class Ligand:
         self.min_docking_score = None
         self.recombinations = []
         self.recombination = recombination
-    def to_sdf(self, sdf_path, pH=7.4):
+    def to_sdf(self, sdf_path):
         """
-        Convert a SMILES string to a sdf file needed by docking programs of the AutoDock family.
+        Writes the current object to a sdf file.
 
         Parameters
         ----------
@@ -186,74 +184,115 @@ class Ligand:
         pH: float
             Protonation at given pH.
         """
-        # TODO whats about PH-protonation
-        # TODO is ROMol_original with dummy?
+
+        # protonate
         molecule = Chem.AddHs(self.ROMol)
+        # 3D generation & optimization of the ligand itself
         status = AllChem.EmbedMolecule(molecule)
         status = AllChem.UFFOptimizeMolecule(molecule)
         w = Chem.SDWriter(str(sdf_path))
         w.write(molecule)
         return
     def add_pose(self, pose : Pose):
+        """
+        Adds a pose to the given ligand.
+
+        Parameters
+        ----------
+        Pose: Pose
+            Pose object that should be added
+        """
         if self.min_docking_score == None or pose.docking_score < self.min_docking_score:
             self.min_docking_score = pose.docking_score
         self.poses.append(pose)
         return
     def choose_template_poses(self, num_templates=None):
         """
-        Chooses *num_templates*-best poses according to docking result and removes the non-top-scored or invalid poses
+        Chooses *num_templates*-best poses according to docking result and diversity and removes the non-top-scored or invalid poses
 
         Parameters
         ----------
         num_templates: int
             max. templates to choose, if None, num_templates == len(path)
-            Path to output file
         """
         if not len(self.poses):
+            # nothing to choose from
             return
         if not num_templates:
+            # default of num_poses = amount of poses
             num_templates = len(self.poses)
-
-        self.poses.sort()
         
-        # Always choose pose with the best docking score
-        choosen_poses = [self.poses[0]]
-        self.poses = self.poses[1:]
+        # always choose the pose with the best docking score
+        choosen_poses = [min(self.poses, key=lambda p: p.docking_score)]
+        self.poses.remove(choosen_poses[0])
+
+        # -----> TODO REMOVE IF FIXED
         for pose in self.poses:
                 if choosen_poses[0].ROMol.GetNumAtoms() != pose.ROMol.GetNumAtoms():
+                    # this should't happen TODO should be fixed by cleaning files
                     print("Couldn't match pose")
                     pose.ROMol = choosen_poses[0].ROMol
+        # -------|
+
+        # define mapping of atoms => avoid a maximal common substructure search (not needed because the structures should be the same)
         atom_mapping = [[j, j] for j in range(choosen_poses[0].ROMol.GetNumAtoms())]
-        for pose in self.poses:
-            if choosen_poses[0].ROMol.GetNumAtoms() != pose.ROMol.GetNumAtoms():
-                print("Couldn't match pose")
-                pose.ROMol = choosen_poses[0].ROMol
-        # Append num_templates - 1 more templates according to highest RMSD to already choosen poses and lowest docking-score
+
+        # append num_templates - 1 more templates according to highest mean of RMSD (to already choosen poses) and the lowest docking-score
         for _ in range(num_templates - 1):
-            # TODO make this more efficient
             if not len(self.poses):
                 break
-            self.poses.sort(key= lambda pose : -2 * sum(rdMolAlign.CalcRMS(pose.ROMol, x.ROMol, map=[atom_mapping]) for x in choosen_poses) / len(choosen_poses) + pose.docking_score)
-            choosen_poses.append(self.poses[0])
-            self.poses = self.poses[1:]
+            # choose minimal pose according to -2 * mean(RMSD to choosen poses) + dockin_score
+            pose = min(self.poses, key=lambda pose : -2 * sum(rdMolAlign.CalcRMS(pose.ROMol, x.ROMol, map=[atom_mapping]) for x in choosen_poses) / len(choosen_poses) + pose.docking_score)
+            choosen_poses.append(pose)
+            self.poses.remove(pose)
+        # overwrite poses with choosen poses
         self.poses = choosen_poses
+
     def calculate_missing_dummy_atoms(self, fragment_library):
+        """
+        Determines dummy atoms that aren't calculated
+
+        Parameters
+        ----------
+        fragment_library: dict
+            Library containing all fragments where the index should match to the fragment ids
+        """
         for subpocket, id in self.fragment_ids.items():
             if subpocket not in self.dummy_atoms.keys():
+                # calculate the dummy atoms if there are no dummy atoms for the subpocket
                 fragment = Chem.RemoveHs(fragment_library[subpocket]['ROMol_original'][id])
                 self.dummy_atoms[subpocket] = [(f"{subpocket}_{i}", a.GetNeighbors()[0].GetProp('environment'), a.GetProp('subpocket')) for i, a in enumerate(fragment.GetAtoms()) if a.GetSymbol() == '*']
+    
     def recombine(self, fragment_id, subpocket, fragment_library):
+        """
+        Recombines the ligand with the fragment (subpocket_fragment_id)
+
+        Returns
+        ----------
+        True if recombination could be found else False
+
+        Parameters
+        ----------
+        fragment_library: dict
+            Library containing all fragments where the index should match to the fragment ids
+        """
+        # make sure that all dummy atoms are added to self.dummy_atoms
         self.calculate_missing_dummy_atoms(fragment_library)
+        # this is need to detemine the correct atom-id
         fragment = Chem.RemoveHs(fragment_library[subpocket]['ROMol_original'][fragment_id])
+        # dummy atoms of the fragment that should to be recombined
         dummy_atoms = [(f"{subpocket}_{i}", a.GetNeighbors()[0].GetProp('environment'), a.GetProp('subpocket')) for i, a in enumerate(fragment.GetAtoms()) if a.GetSymbol() == '*']
         new_rec = None
         for sp in self.dummy_atoms.keys():
-            matching_dummies = [(id, env, con) for id, env, con in dummy_atoms if con == sp]
-            matching_dummies_2 = [(id, env, con) for id, env, con in self.dummy_atoms[sp] if con == subpocket]
+            # for every subpocket (used by ligand): add a connection if valid
+            matching_dummies = [(id, env) for id, env, con in dummy_atoms if con == sp] # dummy atoms of fragment that have a connection to the current subpocket
+            matching_dummies_2 = [(id, env) for id, env, con in self.dummy_atoms[sp] if con == subpocket] # dummy atoms of ligand (in subpocket sb) that have a connection to the subpocket of the fragment
             if len(matching_dummies) != 1 or len(matching_dummies_2) != 1:
+                # if there are more than 1 connection to one subpocket (for now we only allow one connection between subpockets) OR no connection
+                # TODO maybe add all possibities 
                 continue
-            id, env, con = matching_dummies[0]
-            id_2, env_2, con_2 = matching_dummies_2[0]
+            id, env = matching_dummies[0]
+            id_2, env_2 = matching_dummies_2[0]
             if not is_brics_bond(env, env_2):
                 continue
             if new_rec == None:
@@ -265,7 +304,20 @@ class Ligand:
                 self.recombinations.append(new_rec)
                 return True
         return False
-def from_recombination(recombination):
+    
+def from_recombination(recombination) -> Ligand:
+    """
+    Converts a given Recombination-object to a Ligand
+    
+    Returns
+    ----------
+    Ligand
+
+    Parameters
+    ----------
+    recombination: Recombination
+        Recombination that should be converted
+    """
     fragments = {x[:2]: int(x[3:]) for x in recombination.fragments}
     return Ligand(recombination.ligand, fragments, recombination)
 
@@ -275,12 +327,97 @@ class Recombination:
         self.bonds = bonds
         self.fragments = fragment_ids
     def add_fragment(self, fragment_id, bonds):
+        """
+        Adds a fragment given by it's id and bonds
+
+        Parameters
+        ----------
+        fragment_id: str
+            Format: <subpocket>_<id>
+        bonds: List(str)
+            Format of a bond [<subpocket>_<atom_id>, <subpocket>_<atom_id>]
+        """
         if fragment_id not in self.fragments:
             self.fragments.append(fragment_id)
         self.bonds += bonds
     def construct(self, fragment_library):
+        """
+        Constructs a Mol-object from the recombination and stores it
+
+        Parameters
+        ----------
+        fragment_library: Dict
+            Library containing all fragments where the index should match to the fragment ids
+        """
         print("===> recombining:", self.fragments, self.bonds)
         self.ligand = utils.construct_ligand(self.fragments, self.bonds, fragment_library)
     def copy(self):
+        """
+        Copies itself
+
+        Returns
+        ----------
+        New instance of the given recombination
+
+        Parameters
+        ----------
+        fragment_library: Dict
+            Library containing all fragments where the index should match to the fragment ids
+        """
         return Recombination(self.fragments.copy(), self.bonds.copy())
     
+class Filter:
+    def __init__(self, name, params: dict):
+        self.name = name.lower()
+        self.params = params
+    def get_param(self, param_name):
+        """
+        Get a filter-parameter by it's name
+
+        Returns
+        ----------
+        Paramter "param_name" if exists else None
+
+        Parameters
+        ----------
+        param_name: str
+            Name of the parameter
+        """
+        if param_name in self.params.keys():
+            return self.params[param_name]
+        return None
+    def apply_filter(self, fragment_library):
+        """
+        Applies the filter to the given fragment_library
+
+        Returns
+        ----------
+        Filtered library
+
+        Parameters
+        ----------
+        fragment_library: Dict
+            Library containing all fragments where the index should match to the fragment ids
+        """
+        if self.name == 'pains':
+            fragment_library, _ = filters.unwanted_substructures.get_pains(fragment_library)
+        elif self.name == 'brenk':
+            fragment_library, _ = filters.unwanted_substructures.get_brenk(fragment_library, self.get_param('path_data'))
+        elif self.name == 'ro3':
+            fragment_library = filters.drug_likeness.get_ro3_frags(fragment_library)
+        elif self.name == 'qed':
+            fragment_library = filters.drug_likeness.get_qed(fragment_library, cutoff_val=self.get_param('cutoff_val'))
+        elif self.name == 'bb':
+            fragment_library = filters.synthesizability.check_building_blocks(
+                fragment_library,
+                str(str(self.get_param('path_data')) + "/Enamine_Building_Blocks.sdf"),
+            )
+        elif self.name == 'syba':
+            fragment_library = filters.synthesizability.calc_syba(fragment_library, cutoff=self.get_param('cutoff_val'))
+            
+        # remove all filtered fragments from the fragment library
+        for sp in fragment_library.keys():
+            t = fragment_library[sp]['bool_' + self.name]
+            fragment_library[sp] = fragment_library[sp][fragment_library[sp]['bool_' + self.name] == 1]
+        
+        return fragment_library
--- src/fragment_docking.py
+++ src/fragment_docking.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from kinfraglib import utils
+from kinfraglib import utils, filters
 import docking_utils
 from rdkit import Chem
 from pandas import DataFrame
@@ -7,91 +7,158 @@ import pandas as pd
 from brics_rules import is_brics_bond
 from itertools import permutations
 
+import subprocess
+from pathlib import Path
+
+import pandas as pd
+from rdkit.Chem import PandasTools
+import matplotlib.pyplot as plt
+
+
+
 # Definitions
-PATH_TO_LIB = Path('KinFragLib/data/fragment_library')
-PATH_COMBINATORIAL_LIBRARY = 'KinFragLib/data/combinatorial_library/combinatorial_library_deduplicated.json'
-PATH_TO_DOCKING_CONFIGS = Path('docking_config/5l4q')
-PATH_TO_SDF_FRAGMENTS = Path('data/fragments/5l4q/')
-PATH_TO_DOCKING_RESULTS = Path('data/docking/5l4q/')
-PATH_TO_TEMPLATES =  Path('data/templates/5l4q/')
+HERE = Path().resolve()
+PATH_DATA = HERE / "KinFragLib/data"
+PATH_DATA_BRENK = HERE / "KinFragLib/data/filters/Brenk"
+PATH_DATA_ENAMINE = HERE / "KinFragLib/data/filters/DataWarrior"
+PATH_COMBINATORIAL_LIBRARY = HERE /'KinFragLib/data/combinatorial_library/combinatorial_library_deduplicated.json'
+PATH_TO_DOCKING_CONFIGS = HERE / 'docking_config/5l4q'
+PATH_TO_HYDE_CONFIGS = HERE / 'hyde_config/5l4q'
+PATH_TO_SDF_FRAGMENTS = HERE / 'data/fragments/5l4q'
+PATH_TO_DOCKING_RESULTS = HERE / 'data/docking/5l4q'
+PATH_TO_HYDE_RESULTS = HERE / 'data/scoring/5l4q'
+PATH_TO_TEMPLATES =  HERE / 'data/templates/5l4q'
 
-num_ap_fragments = 10
-num_conformers = 5
+num_fragments = 5                 # number of fragments to use 
+num_conformers = 5                  # amount of conformers to choose per docked fragment  (according to docking score and diversity)
+num_fragments_per_iterations = 5  # amount of fragments to choose per docking iteration (according to docking score)
 
+# define filters
+filters_ = []
+
+"""filters_.append(docking_utils.Filter('pains', {}))
+filters_.append(docking_utils.Filter('brenk', {'path_data': PATH_DATA_BRENK}))
+filters_.append(docking_utils.Filter('ro3', {})) # TODO add params
+filters_.append(docking_utils.Filter('qed', {'cutoff_val': 0.492}))
+filters_.append(docking_utils.Filter('bb', {'path_data': PATH_DATA_ENAMINE}))
+filters_.append(docking_utils.Filter('syba', {'cutoff_val': 0}))"""
+
+# define pockets
 core_subpocket = 'AP'
 
 subpockets = ['FP', 'SE']
 
+# ==== PREPROCESSING =======
+
 # Access fragment library
-fragment_library = utils.read_fragment_library(PATH_TO_LIB)
+fragment_library_original = utils.read_fragment_library(PATH_DATA / "fragment_library")
+
+print(str([sp + ': ' + str(len(fragment_library_original[sp])) for sp in fragment_library_original.keys()]))
+
+print('Prefiltering ===>')
+# removing fragments in pool X
+# removing duplicates
+# removing fragments without dummy atoms (unfragmented ligands)
+# removing fragments only connecting to pool X
+fragment_library = filters.prefilters.pre_filters(fragment_library_original)
+print(str([sp + ': ' + str(len(fragment_library[sp])) for sp in fragment_library.keys()]))
+
+# apply filters
+for filter in filters_:
+    print('Filtering: ' + filter.name + ' ===>')
+    l_before = [sp + ': ' + str(len(fragment_library[sp])) for sp in fragment_library.keys()]
+    fragment_library = filter.apply_filter(fragment_library)
+    print(str([sp + ': ' + str(len(fragment_library[sp])) for sp in fragment_library.keys()]))
+
+# ===== CORE DOCKING =======
 
 core_fragments = []
 
-# pre selection  has to be applied here
-for i in fragment_library[core_subpocket][:20].index:
+# prepare all core fragments
+for i in fragment_library[core_subpocket][:num_fragments].index:
     core_fragments.append(docking_utils.Ligand(fragment_library[core_subpocket]['ROMol'][i], {core_subpocket: i}, docking_utils.Recombination([core_subpocket + "_" + str(i)], [])))
 
+# core docking 
 docking_results = []
 
-# core docking
 for core_fragment in core_fragments:
-    # safe fragment as sdf file
     print("=== Docking of " + core_subpocket + "-Fragment: " + str(core_fragment.fragment_ids[core_subpocket]) + " ====")
+    # safe fragment as sdf file
     core_fragment.to_sdf(PATH_TO_SDF_FRAGMENTS / 'core_fragment.sdf')
+
     res = docking_utils.core_docking(PATH_TO_SDF_FRAGMENTS / 'core_fragment.sdf', PATH_TO_DOCKING_CONFIGS / (core_subpocket + '.flexx'), PATH_TO_DOCKING_RESULTS / 'core_fragments.sdf')
-    for conformer in res:
+
+    for conformer in res:   # safe every resulting pose within the fragment
         pose = docking_utils.Pose(conformer, float(conformer.GetProp('BIOSOLVEIT.DOCKING_SCORE')))
         core_fragment.add_pose(pose)
     if len(res):
+        # if fragment could be docked, save fragment (including it's poses)
         print("  best docking score: " + str(core_fragment.min_docking_score))
         docking_results.append(core_fragment)
 
-# Evaluation
-# HYDE TODO
+    # TODO clean files
 
-
-used_subpockets = [core_subpocket]
-subpocket = subpockets[0]
+# ===== TEMPLATE DOCKING ======
 
 for subpocket in subpockets:
 
-    for ligand in docking_results:
-        ligand.choose_template_poses(num_conformers)
+    # filtering
 
     docking_results.sort(key=lambda l: l.min_docking_score)
 
+    # choose n best fragments
+    docking_results = docking_results[:min(len(docking_results), num_fragments_per_iterations)]
+
+    # choose k best conformers (per fragment)
+    for ligand in docking_results:
+        ligand.choose_template_poses(num_conformers)
+
+    # only for evaluation (statistics) purpose 
     with open("statistics.txt", "a") as f:
         for ligand in docking_results:
             f.write(str(list(ligand.fragment_ids.items())) + ": " + str(ligand.min_docking_score) + "\n")
-       
+
+    # recombining
 
     candidates = []
+
+    # try recombine every ligand (comb. of fragmnets) with every fragment of the current subpocket
     for ligand in docking_results:
-        for fragment_idx in fragment_library[subpocket][:20].index:
-            ligand.recombine(fragment_idx, subpocket, fragment_library)
+        for fragment_idx in fragment_library[subpocket][:num_fragments].index:
+            ligand.recombine(fragment_idx, subpocket, fragment_library) # possible recombinations are stored within ligand
         if len(ligand.recombinations):
+            # use a ligand for template docking only if at least one recombination was produced for a ligand
             candidates.append(ligand)
+
+    # template docking
 
     docking_results = []
 
     for ligand in candidates:
         for recombination in ligand.recombinations:
             fragment = docking_utils.from_recombination(recombination)
+            # write recombination that should be docked to file
             fragment.to_sdf(PATH_TO_SDF_FRAGMENTS /(subpocket + '_fragment.sdf'))
+            # for every choosen pose: perform template docking
             for i, pose in enumerate(ligand.poses):
+                # write template to sdf file
                 w = Chem.SDWriter(str(PATH_TO_TEMPLATES / (subpocket + '_fragment.sdf')))  # TODO: sometimes it doesn't write anything to the file
                 w.write(pose.ROMol)
 
+                # template docking (FlexX)
                 print("=== Docking of " + str(list(fragment.fragment_ids.items())) + " Pose: " + str(i) +  " ====")
                 res = docking_utils.template_docking(PATH_TO_SDF_FRAGMENTS / (subpocket + '_fragment.sdf'), PATH_TO_TEMPLATES / (subpocket + '_fragment.sdf'), PATH_TO_DOCKING_CONFIGS / (subpocket + '.flexx'), PATH_TO_DOCKING_RESULTS / ('fragments.sdf'))
+                # safe resulting poses within fragment
                 for conformer in res:
                     pose = docking_utils.Pose(conformer, float(conformer.GetProp('BIOSOLVEIT.DOCKING_SCORE')))
                     fragment.add_pose(pose)
                 if len(res):
                     print("  best docking score: " + str(fragment.min_docking_score))
             if len(fragment.poses):
+                # safe recombination as result only if at least one pose was generated
                 docking_results.append(fragment)
 
+# ===== EVALUATION ======
 if len(docking_results):
     docking_results.sort(key=lambda l: l.min_docking_score)
     with open("statistics.txt", "a") as f:
@@ -99,6 +166,6 @@ if len(docking_results):
             f.write(str(list(ligand.fragment_ids.items())) + ": " + str(ligand.min_docking_score) + "\n")
     print("Best recombination: " + str(list(docking_results[0].fragment_ids.items())) + " Score: " + str(docking_results[0].min_docking_score))
 
-    min_pose = min(docking_results[0].poses)
+    min_pose = min(docking_results[0].poses, key=lambda p: p.docking_score)
     w = Chem.SDWriter(str(PATH_TO_DOCKING_RESULTS / ('final_fragment.sdf')))  # TODO: sometimes it doesn't write anything to the file
     w.write(min_pose.ROMol)
